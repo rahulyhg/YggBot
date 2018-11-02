046ebdda93ad516866356ee9884f6a0220b9148d
AFINC <therealafinc@gmail.com>
2018-11-02 15:48:19 -0400

    stickers now display who sent it

--- index.js
+++ index.js
@@ -33,19 +33,32 @@ client.on('message', message => {
 				.then(msg => console.log('ygg!help requested'))
 				.catch(console.error);
 	}
-	
+
 	//AMAZING!
 	if (message.content === '!amazing') {
 		message.delete(200);
-		message.channel.send(sticker.setImage('https://i.imgur.com/rHm3ZLq.png'))
-			.then(msg => console.log('!amazing requested'))
+		message.channel.send(message.member.nickname + ': ',
+			sticker.setImage('https://i.imgur.com/rHm3ZLq.png'))
+			.then(msg => console.log('!amazing top requested'))
 			.catch(console.error);
 	}
-
+	
+	/*
+	//amazing display at bottom
+	if (message.content === '!amazingbot') {
+		//message.delete(200);
+		message.channel.send(sticker.setFooter(message.author.username).
+			setImage('https://i.imgur.com/rHm3ZLq.png'))
+			.then(msg => console.log('!amazing bottom requested'))
+			.catch(console.error);
+	}
+	*/
+	
 	//sturm dot dot dot
 	if (message.content === '!crickets') {
 		message.delete(200);
-		message.channel.send(sticker.setImage('https://i.imgur.com/Hin7e7g.png'))
+		message.channel.send(message.member.nickname + ': ',
+			sticker.setImage('https://i.imgur.com/Hin7e7g.png'))
 			.then(msg => console.log('!crickets requested'))
 			.catch(console.error);
 	}
@@ -53,7 +66,8 @@ client.on('message', message => {
 	//katalina eh
 	if (message.content === '!eh') {
 		message.delete(200);
-		message.channel.send(sticker.setImage('https://i.imgur.com/GPwncXo.png'))
+		message.channel.send(message.member.nickname + ': ',
+			sticker.setImage('https://i.imgur.com/GPwncXo.png'))
 			.then(msg => console.log('!eh requested'))
 			.catch(console.error);
 	}
@@ -61,7 +75,8 @@ client.on('message', message => {
 	//lyria hi
 	if (message.content === '!hi') {
 		message.delete(200);
-		message.channel.send(sticker.setImage('https://i.imgur.com/sUZQ2bj.png'))
+		message.channel.send(message.member.nickname + ': ',
+			sticker.setImage('https://i.imgur.com/sUZQ2bj.png'))
 			.then(msg => console.log('!hi requested'))
 			.catch(console.error);
 	}
@@ -69,7 +84,8 @@ client.on('message', message => {
 	//vyrn macho
 	if (message.content === '!macho') {
 		message.delete(200);
-		message.channel.send(sticker.setImage('https://i.imgur.com/SCRMPBu.png'))
+		message.channel.send(message.member.nickname + ': ',
+			sticker.setImage('https://i.imgur.com/SCRMPBu.png'))
 			.then(msg => console.log('!macho requested'))
 			.catch(console.error);
 	}
@@ -77,7 +93,8 @@ client.on('message', message => {
 	//charlotta no
 	if (message.content === '!no') {
 		message.delete(200);
-		message.channel.send(sticker.setImage('https://i.imgur.com/XZ3k7EZ.png'))
+		message.channel.send(message.member.nickname + ': ',
+			sticker.setImage('https://i.imgur.com/XZ3k7EZ.png'))
 			.then(msg => console.log('!no requested'))
 			.catch(console.error);
 	}
@@ -85,7 +102,8 @@ client.on('message', message => {
 	//siero despair
 	if (message.content === '!siero') {
 		message.delete(200);
-		message.channel.send(sticker.setImage('https://i.imgur.com/2x3xTGJ.png'))
+		message.channel.send(message.member.nickname + ': ',
+			sticker.setImage('https://i.imgur.com/2x3xTGJ.png'))
 			.then(msg => console.log('!siero requested'))
 			.catch(console.error);
 	}
@@ -93,7 +111,8 @@ client.on('message', message => {
 	//metera bad slap
 	if (message.content === '!slap') {
 		message.delete(200);
-		message.channel.send(sticker.setImage('https://i.imgur.com/2BCeOLr.png'))
+		message.channel.send(message.member.nickname + ': ',
+			sticker.setImage('https://i.imgur.com/2BCeOLr.png'))
 			.then(msg => console.log('!slap requested'))
 			.catch(console.error);
 	}
@@ -101,7 +120,8 @@ client.on('message', message => {
 	//bea thwump
 	if (message.content === '!thwump') {
 		message.delete(200);
-		message.channel.send(sticker.setImage('https://i.imgur.com/JbtWPhJ.png'))
+		message.channel.send(message.member.nickname + ': ',
+			sticker.setImage('https://i.imgur.com/JbtWPhJ.png'))
 			.then(msg => console.log('!thwump requested'))
 			.catch(console.error);
 	}
@@ -109,7 +129,8 @@ client.on('message', message => {
 	//yuel what's up
 	if (message.content === '!whatsup') {
 		message.delete(200);
-		message.channel.send(sticker.setImage('https://i.imgur.com/K4bUprm.png'))
+		message.channel.send(message.member.nickname + ': ',
+			sticker.setImage('https://i.imgur.com/K4bUprm.png'))
 			.then(msg => console.log('!whatsup requested'))
 			.catch(console.error);
 	}
@@ -118,7 +139,8 @@ client.on('message', message => {
 	//bea why me
 	if (message.content === '!whyme') {
 		message.delete(200);
-		message.channel.send(sticker.setImage('https://i.imgur.com/tI87VJS.png'))
+		message.channel.send(message.member.nickname + ': ',
+			sticker.setImage('https://i.imgur.com/tI87VJS.png'))
 			.then(msg => console.log('!whyme requested'))
 			.catch(console.error);
 	}
@@ -126,11 +148,12 @@ client.on('message', message => {
 	//vira wiggle
 	if (message.content === '!wiggle') {
 		message.delete(200);
-		message.channel.send(sticker.setImage('https://i.imgur.com/ekY0HKm.png'))
+		message.channel.send(message.member.nickname + ': ',
+			sticker.setImage('https://i.imgur.com/ekY0HKm.png'))
 			.then(msg => console.log('!wiggle requested'))
 			.catch(console.error);
 	}
 	
 });
 
-client.login('token-goes-here');
+client.login('tokengoeshere');
