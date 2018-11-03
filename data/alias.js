9d379b3be762b8dd299845ce81235fcad0a0d784
AFINC <therealafinc@gmail.com>
2018-11-02 23:55:41 -0400

    Add files via upload

---
+++ data/alias.js
@@ -0,0 +1,106 @@
+/* Contains aliases for characters
+*/
+const alias = {
+	//memes
+	'nezahualpilli' : 'birdman',
+	'dokkan' : 'clarisse',
+	'ladykatapillar' : 'katapillar',
+	'batman' : 'siegfried',
+	
+	//alt elements
+	'd jeanne' : 'jeanne-dark',
+	'djeanne' : 'jeanne-dark',
+	'fire clarisse' : 'clarisse',
+	'light clarisse' : 'clarisse-light',
+	'water lancelot' : 'lancelot',
+	'wind lancelot' : 'lancelot-wind',
+	'wind metera' : 'metera',
+	'fire metera' : 'metera-fire',
+	'light sarunan' : 'sarunan',
+	'dark sarunan' : 'sarunan',
+	'water societte' : 'societte',
+	'fire societte' : 'societte-fire',
+	'dark vaseraga' : 'vaseraga',
+	'earth vaseraga' : 'vaseraga-earth',
+	'fire yuel' : 'yuel',
+	'water yuel' : 'yuel-water',
+	'fire zeta' : 'zeta',
+	'dark zeta' : 'zeta-dark',
+	'earth cagliostro' : 'cagliostro',
+	'dark cagliostro' : 'cagliostro',
+
+	//nicknames
+	'lance' : 'lancelot',
+	'perci' : 'percival',
+	'arulu' : 'arulumaya',
+	'dijon' : 'jeanne-dark',
+	'yoda' : 'yodarha',
+	'zahlhamelina' : 'zahl',
+	'gun zooey' : 'zooey',
+	
+	'dlf' : 'delafille',
+	'earth dlf' : 'delafille-earth',
+	'saru' : 'sarunan',
+	'light saru' : 'sarunan',
+	'dark saru' : 'sarunan-dark',
+	'vase' : 'vaseraga',
+	'dark vase' : 'vaseraga',
+	'earth vase' : 'vaseraga-earth',
+	'earth cag' : 'cagliostro',
+	'dark cag' : 'cagliostro-dark',
+	
+	'dorothy' : 'maids',
+	'claudia' : 'maids',
+	'dorothy and claudia' : 'maids',
+	'wulf and renie' : 'wulf',
+	'renie' : 'wulf',
+	
+	//holiday
+	'christmas arulu' : 'arulumaya-c',
+	'christmas clarisse' : 'clarisse-c',
+	'christmas mary' : 'mary-c',
+	'christmas rosetta' : 'rosetta-c',
+	
+	//halloween
+	'halloween danua' : 'danua-h',
+	'halloween cagliostro' : 'cagliostro-h',
+	'halloween cag' : 'cagliostro-h',
+	'halloween charlotta' : 'charlotta-h',
+	'halloween eustace' : 'eustace-h',
+	
+	//summer/swimsuits
+	'summer bea' : 'beatrix-ss',
+	'summer beatrix' : 'beatrix-ss',
+	'summer danua' : 'danua-ss',
+	'summer de la fille' : 'delafille-ss',
+	'summer dlf' : 'delafille-ss',
+	'summer diantha' : 'diantha-ss',
+	'summer grea' : 'grea-ss',
+	'summer heles' : 'heles-ss',
+	'summer io' : 'io-ss',
+	'summer izmir' : 'izmir-ss',
+	'summer jeanne' : 'jeanne-ss',
+	'summer korwa' : 'korwa-ss',
+	'summer naoise' : 'naoise-ss',
+	'summer narmaya' : 'narmaya-ss',
+	'summer percival' : 'percival-ss',
+	'summer rosetta' : 'rosetta-ss',
+	'summer siegfried' : 'siegfried-ss',
+	'summer vira' : 'vira-ss',
+	'summer zeta' : 'zeta-ss',
+	'summer zooey' : 'zooey-grand',
+	
+	//eternals
+	'uno' : 'anre',
+	'song' : 'tweyen',
+	'sarasa' : 'threo',
+	'quatre' : 'feower',
+	'funf' : 'fif',
+	'six' : 'seox',
+	'siete' : 'seofon',
+	'octo' : 'eahta',
+	'nio' : 'niyon',
+	'esser' : 'tien'
+};
+
+module.exports = alias;
